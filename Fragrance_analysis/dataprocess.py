7395247de7694d6898667a9131564cc54d515d4f
David <1697252093@qq.com>
2025-01-11 23:15:53 +0800

    update the anova analysis

--- Fragrance_analysis/dataprocess.py
+++ Fragrance_analysis/dataprocess.py
@@ -1,13 +1,17 @@
 import pandas as pd
 
 
-df1 =  pd.read_csv('data/归一化的100hz SCL特征/filtered_scl_features.csv')
-df2 =  pd.read_csv('data/归一化的100hz SCL特征/phasic_scl_features.csv')
-df3 =  pd.read_csv('data/归一化的100hz SCL特征/tonic_scl_features.csv')
+df1 = pd.read_excel('Fragrance_analysis/data/experiment.xlsx')
+df2 = pd.read_csv('Fragrance_analysis/data/new_table.csv')
 
 
-# 按照file 和 segment 合并
-df = pd.merge(df1, df2, on=['File', 'Segment'])
-df = pd.merge(df, df3, on=['File', 'Segment'])
+# 选择'试次'，'P1', 'A1', 'D1', 'P2', 'A2', 'D2', 'P3', 'A3', 'D3' 这几列
 
-df.to_csv('data/归一化的100hz SCL特征/merged_scl_features.csv', index=False)
+df1 = df1[['试次', 'P1', 'A1', 'D1', 'P2', 'A2', 'D2', 'P3', 'A3', 'D3']]
+
+# 将df1的试次列名改为file
+df1.rename(columns={'试次': 'file'}, inplace=True)
+
+df = pd.merge(df1, df2, on='file')
+
+df.to_csv('Fragrance_analysis/data/new_table.csv', index=False)
